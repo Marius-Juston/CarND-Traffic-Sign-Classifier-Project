2a4f88a1a5b4a9405b05e7d2a53b9da373491fcd
Marius-Juston <Marius.juston@hotmail.fr>
2020-05-31 17:44:24 -0400

    created image and images pipeline

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -5,6 +5,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 from skimage import exposure
 
+
+def preprocess(image, clip_limit=.1, h=20, h_color=20, template_window_size=3, search_window_size=3):
+    image = exposure.equalize_adapthist(image, clip_limit=clip_limit).astype('float32')
+    image = np.uint8(image * 255)
+    image = cv2.fastNlMeansDenoisingColored(image, None, h, h_color, template_window_size, search_window_size)
+    image = cv2.normalize(image, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+
+    return image
+
+
+def preprocess_images(images):
+    # return np.fromiter((preprocess(image) for image in images), dtype=np.float32)
+    return np.array(list(preprocess(image) for image in images))
+
+
 if __name__ == '__main__':
     training_file = 'data/train.p'
     validation_file = 'data/valid.p'
