d8ee6c058461fa774e67081884fe9c96577525c9
Marius-Juston <Marius.juston@hotmail.fr>
2020-05-31 17:45:15 -0400

    removed unecessary file loading

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -22,53 +22,29 @@ def preprocess_images(images):
 
 if __name__ == '__main__':
     training_file = 'data/train.p'
-    validation_file = 'data/valid.p'
-    testing_file = 'data/test.p'
 
     with open(training_file, mode='rb') as f:
         train = pickle.load(f)
-    with open(validation_file, mode='rb') as f:
-        valid = pickle.load(f)
-    with open(testing_file, mode='rb') as f:
-        test = pickle.load(f)
 
     X_train, y_train = train['features'], train['labels']
-    X_valid, y_valid = valid['features'], valid['labels']
-    X_test, y_test = test['features'], test['labels']
 
-    # image = X_train[342]
-    # image = X_train[320]
-    # image = X_train[543]
-    # image = X_train[1543]
-    # image = X_train[6522]
-    # image = X_train[8522]
-    # image = X_train[14832]
-    # image = X_train[32000]
-    # image = X_train[32834]
-    # image = X_train[33834]
-    # image = X_train[900]
-    # image = X_train[1200]
-    # image = X_train[2000]
-    # image = X_train[2200]
-    image = X_train[2400]
-    print(y_train[2400])
+    index = 30552
+
+    image = X_train[index]
+    print(y_train[index])
 
     plt.imshow(image)
     plt.show()
 
-    histo_gram = exposure.equalize_adapthist(image, clip_limit=0.1)
+    image = preprocess(image)
 
-    plt.imshow(histo_gram)
+    plt.imshow(image)
     plt.show()
 
-    color_image = histo_gram.astype('float32')
+    images = preprocess_images(X_train)
 
-    p_image = np.uint8(color_image * 255)
-
-    # plt.imshow(p_image)
-    # plt.show()
-
-    noised = cv2.fastNlMeansDenoisingColored(p_image, None, 20, 20, 3, 3)
-    #
-    plt.imshow(noised)
+    plt.imshow(image[index])
     plt.show()
+
+    print(images.shape)
+    print(images.dtype)
